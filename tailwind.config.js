66ead51f18b6b6f73428379741c3cdb463c96755
Marvin <sanchezmarvin38@gmail.com>
2024-08-13 12:42:34 +0200

    feat: add right and down directions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}', "./node_modules/svhighlight/**/*.svelte"],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    './node_modules/svhighlight/**/*.svelte',
+  ],
   theme: {
     extend: {
       keyframes: {
-        "marquee-left": {
-          from: { transform: "translateX(0)" },
-          to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        'marquee-left': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(calc(-100% - var(--gap)))' },
         },
-        "marquee-up": {
-          from: { transform: "translateY(0)" },
-          to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        'marquee-right': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(calc(100% + var(--gap)))' },
+        },
+        'marquee-up': {
+          from: { transform: 'translateY(0)' },
+          to: { transform: 'translateY(calc(-100% - var(--gap)))' },
+        },
+        'marquee-down': {
+          from: { transform: 'translateY(0)' },
+          to: { transform: 'translateY(calc(100% + var(--gap)))' },
         },
       },
       animation: {
-        "marquee-left": "marquee-left var(--duration, 40s) linear infinite",
-        "marquee-up": "marquee-up var(--duration, 40s) linear infinite",
+        'marquee-left': 'marquee-left var(--duration, 40s) linear infinite',
+        'marquee-right': 'marquee-right var(--duration, 40s) linear infinite',
+        'marquee-up': 'marquee-up var(--duration, 40s) linear infinite',
+        'marquee-down': 'marquee-down var(--duration, 40s) linear infinite',
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-  ],
-}
-
+  plugins: [require('tailwindcss-animate')],
+};
